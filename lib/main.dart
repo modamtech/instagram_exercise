353bf507f7015c8de4c37164fae143d3cfa27f20
modamtech <modamtech@gmail.com>
2023-02-06 07:19:07 +0900

    2023.02.04 파일분리 예제

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'sub/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,29 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(CupertinoIcons.camera, color: Colors.black),
-          onPressed: () {},
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(CupertinoIcons.paperplane, color: Colors.black),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: Center(child: Text("home page")),
     );
   }
 }
